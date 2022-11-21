86a4d5305142daff6c8cbb3599dbe90e0be7dedc
Vinicius Mitsuo Carvalho Carrion Moises <37308081+vinicarrion@users.noreply.github.com>
2022-11-21 17:23:06 -0300

    Update Startup.cs

--- TrabalhoImobi/TrabalhoImobi/Startup.cs
+++ TrabalhoImobi/TrabalhoImobi/Startup.cs
@@ -27,11 +27,17 @@ namespace TrabalhoImobi
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<UsuarioContext>(options => options.UseMySQL(
+                Configuration.GetConnectionString("CardConnection")));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TrabalhoImobi", Version = "v1" });
-            });
+            
+            services.AddDbContext<Fundos_ImobiliarioContext>(options => options.UseMySQL(
+                Configuration.GetConnectionString("CardConnection")));
+            services.AddControllers();
+            
+            services.AddDbContext<UsuarioContext>(options => options.UseMySQL(
+                Configuration.GetConnectionString("CardConnection")));
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
