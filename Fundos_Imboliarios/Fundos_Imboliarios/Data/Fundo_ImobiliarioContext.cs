1075a96cc4c6cef13531af1852a15d41a3573f6c
Vinicius Mitsuo Carvalho Carrion Moises <37308081+vinicarrion@users.noreply.github.com>
2022-11-21 21:34:19 -0300

    Add files via upload

---
+++ Fundos_Imboliarios/Fundos_Imboliarios/Data/Fundo_ImobiliarioContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Fundos_Imboliarios.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fundos_Imboliarios.Data
+{
+    public class Fundo_ImobiliarioContext : DbContext
+    {
+        public Fundo_ImobiliarioContext(DbContextOptions<Fundo_ImobiliarioContext> opt) : base(opt)
+        {
+        }
+        public DbSet<Model.Fundos_Imboliario> Fundos_Imobiliarios { get; set; }
+    }
+
+}
