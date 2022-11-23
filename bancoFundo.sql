60af58d2b1d31040fe80d1b3a06c5957c3f7cf60
Vinicius Mitsuo Carvalho Carrion Moises <37308081+vinicarrion@users.noreply.github.com>
2022-11-23 13:49:08 -0300

    Update bancoFundo.sql

--- bancoFundo.sql
+++ bancoFundo.sql
@@ -9,23 +9,23 @@ email varchar(100) not null,
 senha varchar(100) not null
 );
 
-create table tb_carteira(
-id_carteira integer auto_increment primary key,
+create table carteiras(
+id integer auto_increment primary key,
 fiis varchar(7),
 id_usuario integer,
-FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
 );
 
-create table tb_fiis(
-id_fiis integer auto_increment primary key,
+create table fundos(
+id integer auto_increment primary key,
 dividendo double not null,
 adm varchar(100)not null,
 tipo varchar(30) not null,
 area varchar(50) not null,
 desp_adm double not null,
 id_usuario integer,
-FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario),
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id),
 id_carteira integer,
-FOREIGN KEY (id_carteira) REFERENCES tb_carteira(id_carteira)
+FOREIGN KEY (id_carteira) REFERENCES carteiras(id)
 );
 
